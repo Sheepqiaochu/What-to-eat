a13eb201f7149488ddd72b7440782ec7172b317d
2638585108-ms <58544427+2638585108-ms@users.noreply.github.com>
2019-12-06 23:51:53 +0800

    Update Evaluate.java

--- Evaluate.java
+++ Evaluate.java
@@ -1,18 +1,18 @@
 package WTE;
 
-public class Evaluate {
-private String UID;
-private Double point[];
+public class Evaluate {			//用户评价类
+private String UID;				//用户ID
+private Double point[];			//评价值数组
 
-public String getid() {
+public String getid() {			//获取id
 	return UID;
 }
 
-public Double getpoint(int j) {
+public Double getpoint(int j) {		//获取某一物品的评价
 	return point[j];
 }
 
-public Evaluate(User p,Double po[]) {
+public Evaluate(User p,Double po[]) {		//构造函数
 	this.UID = p.getid();
 	point = new Double[po.length];
 	for(int i=0;i<po.length;i++)
